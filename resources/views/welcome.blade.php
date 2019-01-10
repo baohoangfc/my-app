11169609b20ebfb8040d36649a21f20f66793cc2
baohoangfc <baohoangfc@gmail.com>
2019-01-10 13:28:54 +0700

    baobui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -89,7 +89,7 @@
                     <a href="https://laracasts.com">Laracasts122132131313</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Novava</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://forge.laravel.com">Forgev</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
